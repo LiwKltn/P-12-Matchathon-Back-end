c8941ada92512f5f8dc818cfd806adbbcbc552f2
Oliwia Ann Koltun <144242610+LiwKltn@users.noreply.github.com>
2024-04-11 09:50:24 +0200

    fix errors of genders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
-            GenderSeeder::class,
             BootcampSeeder::class,
             StackSeeder::class,
             LevelSeeder::class,
