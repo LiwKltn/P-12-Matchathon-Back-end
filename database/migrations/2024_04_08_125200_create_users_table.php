3eb2742e544360604981f12d57d943aa6e9cd35f
Oliwia Ann Koltun <144242610+LiwKltn@users.noreply.github.com>
2024-04-11 09:42:09 +0200

    deletede genders

--- database/migrations/2024_04_08_125200_create_users_table.php
+++ database/migrations/2024_04_08_125200_create_users_table.php
@@ -18,10 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->unsignedBigInteger('bootcamp_id');
-            $table->unsignedBigInteger('gender_id');
             $table->unsignedBigInteger('role_id');
             $table->foreign('bootcamp_id')->references('id')->on('bootcamps');
-            $table->foreign('gender_id')->references('id')->on('genders');
             $table->foreign('role_id')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
